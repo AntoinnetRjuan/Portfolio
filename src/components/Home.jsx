02752326c94dbdb2b8c36115daa30650712d74c4
AntoinnetRjuan <168259860+AntoinnetRjuan@users.noreply.github.com>
2024-08-29 13:55:34 +0300

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-
+import me from '../images/profil.jpg'
 export default function Home(){
     return(
         <>
@@ -7,7 +7,7 @@ export default function Home(){
                 <div className="Propos">
                     <h2>I am RATSARAFARA Jean Antoinnet</h2>
                     <h3>Developer</h3>
-                    <img src="src\images\profil.jpg"/>
+                    <img src={me}/>
                 </div>
                 
             </div>
